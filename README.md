7bac73faaa18c20d028f31d10b433fc316080950
Erik J. Sturcke <erik@sturcke.co>
2022-06-17 10:18:31 -0400

    Add option to see CPU usage

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Hog
 
-This tool leverages `top` and `ps` to get a rough idea of what applications are using excessive memory.
+This tool leverages `top` and `ps` to get a rough idea of what applications are using excessive memory or CPU.
 
 <img width="307" alt="Screen Shot 2022-06-08 at 13 14 27" src="https://user-images.githubusercontent.com/459656/172677246-21ce33aa-b440-4a04-a90d-fccb5cadaa61.png">
 
@@ -8,19 +8,19 @@ This tool leverages `top` and `ps` to get a rough idea of what applications are 
 
 ```
 brew install deno
-deno run --allow-run=top,ps --check https://deno.land/x/hog@0.0.4/hog.ts
+deno run --allow-run=top,ps --check https://deno.land/x/hog@0.0.5/hog.ts
 ```
 
 # Installing
 
 ```
-deno install --allow-run=top,ps https://deno.land/x/hog@0.0.4/hog.ts
+deno install --allow-run=top,ps https://deno.land/x/hog@0.0.5/hog.ts
 ```
 
 or to update
 
 ```
-deno install -f --allow-run=top,ps https://deno.land/x/hog@0.0.4/hog.ts
+deno install -f --allow-run=top,ps https://deno.land/x/hog@0.0.5/hog.ts
 ```
 
 # Running
@@ -33,4 +33,16 @@ to show PIDs for top level processes that were collapsed by name:
 
 ```
 hog -p
+```
+
+to see CPU usage
+
+```
+hog cpu
+```
+
+note that this will take a few seconds as the default is to take 4 samples. To get CPU usage over more samples:
+
+```
+hog cpu -s 20
 ```
--- hog.ts
+++ hog.ts
@@ -10,7 +10,7 @@ import { parse } from "https://deno.land/std@0.143.0/flags/mod.ts";
 type Pid = string;
 
 const parseMemory = (mem: string) => {
-  const match = mem.match(/^(\d+)(K|M|G)$/);
+  const match = mem.match(/^(\d+)(K|M|G)[-+]?$/);
   if (!match) throw Error(`Could not parse memory ${mem}`);
   const scale =
     match[2] === "K"
@@ -54,9 +54,10 @@ const applicationNames = async () => {
   const lines = new TextDecoder().decode(await process.output()).split("\n");
   process.close();
 
+  let skipping = true;
   for (const line of lines) {
     if (skipping) {
-      if (line.startsWith("PID")) skipping = false;
+      if (line.trimStart().startsWith("PID")) skipping = false;
       continue;
     }
     if (line === "") continue;
@@ -68,60 +69,88 @@ const applicationNames = async () => {
   return names;
 };
 
-// Get process memory usage
-const process = Deno.run({
-  cmd: ["top", "-stats", "ppid,pid,mem", "-l", "1"],
-  stdout: "piped",
-});
+// Get usage
+const getProcessTree = async ({ samples = 1 }: { samples?: number }) => {
+  const process = Deno.run({
+    cmd: ["top", "-stats", "ppid,pid,mem,cpu", "-a", "-l", String(samples)],
+    stdout: "piped",
+  });
 
-const lines = new TextDecoder().decode(await process.output()).split("\n");
-process.close();
+  const lines = new TextDecoder().decode(await process.output()).split("\n");
+  process.close();
 
-const processTree = new Map<
-  string,
-  { children: Set<string>; memory: number }
->();
+  const processTree = new Map<
+    string,
+    { children: Set<string>; memory: number; cpu: number }
+  >();
 
-let skipping = true;
-for (const line of lines) {
-  if (skipping) {
-    if (line.startsWith("PPID")) skipping = false;
-    continue;
+  let skipping = true;
+  let skippedSamples = 0;
+  for (const line of lines) {
+    if (skipping) {
+      if (line.startsWith("PPID")) {
+        skippedSamples++;
+        if (skippedSamples === samples) skipping = false;
+      }
+      continue;
+    }
+    if (line === "") continue;
+
+    const [ppid, pid, memory, cpu] = line.split(/\s+/);
+
+    if (!processTree.has(ppid))
+      processTree.set(ppid, { children: new Set(), memory: 0, cpu: 0 });
+    if (!processTree.has(pid))
+      processTree.set(pid, { children: new Set(), memory: 0, cpu: 0 });
+
+    processTree.get(ppid)!.children.add(pid);
+    processTree.get(pid)!.memory = parseMemory(memory);
+    processTree.get(pid)!.cpu = parseFloat(cpu);
   }
-  if (line === "") continue;
-  const [ppid, pid, mem] = line.split(/\s+/);
-  const memory = parseMemory(mem);
 
-  if (!processTree.has(ppid))
-    processTree.set(ppid, { children: new Set(), memory: 0 });
-  if (!processTree.has(pid))
-    processTree.set(pid, { children: new Set(), memory: 0 });
-
-  processTree.get(ppid)!.children.add(pid);
-  processTree.get(pid)!.memory = memory;
-}
-
-// Sum up memory
-const totalMemory = (pid: string) => {
-  let sum = 0;
-  const { children, memory } = processTree.get(pid)!;
-  sum += memory;
-  for (const child of children) sum += totalMemory(child);
-  return sum;
+  return processTree;
 };
 
 // Get application names
-const names = await applicationNames();
+const getApplications = async ({ samples }: { samples?: number } = {}) => {
+  const [names, processTree] = await Promise.all([
+    applicationNames(),
+    getProcessTree({ samples }),
+  ]);
 
-const applications = new Map<string, { memory: number; pids: Set<string> }>();
-for (const pid of processTree.get("1")!.children) {
-  const name = names.get(pid)?.shortName;
-  if (name == null) continue;
-  if (!applications.has(name))
-    applications.set(name, { memory: 0, pids: new Set() });
-  applications.get(name)!.memory += totalMemory(pid);
-  applications.get(name)!.pids.add(pid);
-}
+  // Sum up memory
+  const totalMemory = (pid: string) => {
+    let sum = 0;
+    const { children, memory } = processTree.get(pid)!;
+    sum += memory;
+    for (const child of children) sum += totalMemory(child);
+    return sum;
+  };
+
+  // Sum up cpu
+  const totalCpu = (pid: string) => {
+    let sum = 0;
+    const { children, cpu } = processTree.get(pid)!;
+    sum += cpu;
+    for (const child of children) sum += totalCpu(child);
+    return sum;
+  };
+
+  const applications = new Map<
+    string,
+    { memory: number; cpu: number; pids: Set<string> }
+  >();
+  for (const pid of processTree.get("1")!.children) {
+    const name = names.get(pid)?.shortName;
+    if (name == null) continue;
+    if (!applications.has(name))
+      applications.set(name, { memory: 0, cpu: 0, pids: new Set() });
+    applications.get(name)!.memory += totalMemory(pid);
+    applications.get(name)!.cpu += totalCpu(pid);
+    applications.get(name)!.pids.add(pid);
+  }
+  return applications;
+};
 
 const formatMemory = (memory: number) => {
   if (memory > 2 ** 20 * 0.9)
@@ -133,13 +162,35 @@ const formatMemory = (memory: number) => {
   return brightBlue(sprintf("%5.1f KB", memory));
 };
 
-const args = parse(Deno.args);
+const formatCpu = (cpu: number) => {
+  if (cpu > 80) return brightRed(sprintf("%5.1f %%", cpu));
+  if (cpu > 30 ** 10 * 150) return brightYellow(sprintf("%5.1f %%", cpu));
+  return brightBlue(sprintf("%5.1f %%", cpu));
+};
 
-for (const [name, { memory, pids }] of [...applications]
-  .filter((a) => a[1].memory > 2 ** 10 * 50)
-  .sort((a, b) => b[1].memory - a[1].memory)) {
-  console.log(
-    `${formatMemory(memory)}  ${name}` +
-      (args.p ? ` ${gray("(" + [...pids].join(", ") + ")")}` : "")
-  );
+const args = parse(Deno.args);
+const mode = args._[0] === "cpu" ? "cpu" : "memory";
+
+if (mode === "cpu") {
+  for (const [name, { cpu, pids }] of [
+    ...(await getApplications({
+      samples: args.s ? parseInt(args.s, 10) : 4,
+    })),
+  ]
+    .filter((a) => a[1].cpu > 1)
+    .sort((a, b) => b[1].cpu - a[1].cpu)) {
+    console.log(
+      `${formatCpu(cpu)}  ${name}` +
+        (args.p ? ` ${gray("(" + [...pids].join(", ") + ")")}` : "")
+    );
+  }
+} else {
+  for (const [name, { memory, pids }] of [...(await getApplications())]
+    .filter((a) => a[1].memory > 2 ** 10 * 50)
+    .sort((a, b) => b[1].memory - a[1].memory)) {
+    console.log(
+      `${formatMemory(memory)}  ${name}` +
+        (args.p ? ` ${gray("(" + [...pids].join(", ") + ")")}` : "")
+    );
+  }
 }
